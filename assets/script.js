a19827743350f38cc1e6a8dbeaa498a929b59c4f
Ikram Bchina <bchina.ikram@gmail.com>
2024-03-19 12:08:26 +0100

    create: updateCarousel

--- assets/script.js
+++ assets/script.js
@@ -20,8 +20,9 @@ const slides = [
 const left = document.querySelector('.arrow_left');
 const right = document.querySelector('.arrow_right');
 const dots = document.querySelectorAll('.dot'); //Sélectionnez tous les points 
+const bannerImg = document.querySelector('.banner-img');
 
-
+let currentIndex = 0;
 
 // Fonction pour mettre à jour les points indicateurs
 function updateDots(index) {
@@ -33,6 +34,40 @@ function updateDots(index) {
         }
     });
 }
+
+
+// Fonction pour mettre à jour les points indicateurs, l'image et le texte
+function updateCarousel(index, direction) {
+    //correction du bug pour la première et la dernière image
+    if (currentIndex === -1 && direction === 'left') {
+      currentIndex = slides.length - 1;
+  } else if (currentIndex === slides.length && direction === 'right') {
+      currentIndex = 0;
+  }
+  // Mettre à jour l'image
+const imagePath = `assets/images/slideshow/${slides[currentIndex].image}`;
+bannerImg.src = imagePath;
+bannerImg.alt = `Slide ${currentIndex + 1}`;
+// Mettre à jour le texte
+const tagLine = slides[currentIndex].tagLine;
+document.querySelector('p').innerHTML = tagLine;
+
+console.log(`Clic sur la flèche ${direction}`);
+}
+
+// Gestionnaire d'événement pour le clic sur la flèche gauche
+left.addEventListener('click', function () {
+currentIndex = (currentIndex - 1);
+updateCarousel(currentIndex, 'left');
+updateDots(currentIndex); // Mettez à jour les points indicateurs
+});
+
+// Gestionnaire d'événement pour le clic sur la flèche droite
+right.addEventListener('click', function () {
+currentIndex = (currentIndex + 1) ;
+updateCarousel(currentIndex, 'right');
+updateDots(currentIndex); // Mettez à jour les points indicateurs
+});
 
 
 
